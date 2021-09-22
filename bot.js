5971aeed76ef22214d135115e20d23674c034959
A. Tricot <al.tricot1@gmail.com>
2021-09-23 01:13:57 +0200

    First try of the riot API.

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const dotenv = require('dotenv');
 const { Client, Intents } = require('discord.js');
 const handleMessage = require('./handleMessage');
+const LeagueJS = require('leaguejs/lib/LeagueJS');
 
 // load the bot token from .env
 dotenv.config();
+
+// Discord bot interactions
 
 // load a new client
 const client = new Client({
@@ -13,13 +16,31 @@ const client = new Client({
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
     ],
 });
-
 // print when ready
 client.once('ready', () => {
     console.log('Ready!');
 });
-
-client.on('messageCreate', handleMessage);
-
 // login
 client.login(process.env.DISCORD_TOKEN);
+// message handling
+client.on('messageCreate', handleMessage);
+
+// Riot API interactions
+const leagueJs = new LeagueJS(process.env.RIOT_API_KEY, {
+    PLATFORM_ID: process.env.LEAGUE_API_PLATFORM_ID,
+});
+
+leagueJs.Summoner.gettingByName('Hgis').then((summ) => {
+    const hgis = summ;
+    console.log('\n Hgis : \n');
+    console.log(hgis);
+    leagueJs.Match.gettingListByAccount(hgis?.accountId).then((data) => {
+        const last_match = data['matches'][0];
+        console.log('\n last match :\n');
+        console.log(last_match);
+        leagueJs.StaticData.gettingChampionById(last_match['champion']).then((champ) => {
+            console.log('\n champion : \n');
+            console.log(champ);
+        });
+    });
+});
---
+++ todo.md
@@ -0,0 +1,32 @@
+# BolLot
+
+## ToDos
+
+- [ ] finally learn how to use promises
+- [c] explore the LeagueApi docs
+- [ ] or use LeagueJS which seems overall better : https://www.npmjs.com/package/leaguejs
+- [ ] define the different projects to build
+- [ ] figure out how to get all users connected in the Discord channel
+- [ ] associate them to their league accounts ?
+- [ ] or just have a lost of tracked accounts, which can be added/removed
+    using Discord messages
+- [ ] figure out how to execute a task on a regular basis
+- [ ] figure out how to get info on the current game of a player
+- [ ] define the architecture of the current match(es) tracker
+
+## Architecture
+
+### Current match tracker
+
+The idea is to have a list of players followed (either the connected players or
+a list of players managed from the Discord channel) and output the rank and
+champions win rate of all players in the game when one (or more) of the players
+enter a game.
+
+This requires:
+
+- a way to track a bunch of players by name or puuid
+- a way to execute a task on a regular basis (request the current game)
+- a way to get the current game
+- a way to get the passed games of a player
+- a way to print a bunch of info cleanly on Discord (embed msgs ?)
