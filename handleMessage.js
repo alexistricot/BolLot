7f6339aa07d00340d004288a922b62a4042aad14
A. Tricot <al.tricot1@gmail.com>
2021-09-30 23:29:09 +0200

    Refactor the message handling function.

--- handleMessage.js
+++ handleMessage.js
@@ -22,25 +22,10 @@ function lastGameBySummonerName(message, leagueJs, summonerName) {
     leagueJs.Summoner.gettingByName(summonerName).then((summ) => {
         leagueJs.Match.gettingListByAccount(summ.puuid, { beginIndex: 0, endIndex: 1 }).then(
             (data) => {
-                // initialize output
-                let output = '';
                 // get the last game
                 const last_match = data['matches'][0];
                 console.log(last_match['metadata']);
-                // print match metadata
-                const date = matchDate(last_match);
-                const winID = winningTeamID(last_match);
-                output += `Game from ${date}, `;
-                output += `${String(config['teams'][winID])} won\n\n`;
-                // print participants KDA
-                const participants = last_match['info']['participants'];
-                for (const player of participants) {
-                    const team = config['teams'][String(player['teamId'])];
-                    output += `${team} **${player['summonerName']}** *${player['championName']}* `;
-                    output += `${player['kills']}/${player['deaths']}/${player['assists']}`;
-                    output += '\n';
-                }
-                message.reply(output);
+                message.reply(matchToString(last_match));
             },
         );
     });
@@ -53,4 +38,23 @@ function matchDate(match) {
 function winningTeamID(match) {
     if (match['info']['teams'][0]['win']) return match['info']['teams'][0]['teamId'];
     else return match['info']['teams'][1]['teamId'];
+}
+
+function matchToString(match) {
+    // initialize output
+    let output = '';
+    // match metadata
+    const date = matchDate(match);
+    const winID = winningTeamID(match);
+    output += `Game from ${date}, `;
+    output += `${String(config['teams'][winID])} won\n\n`;
+    // participants KDA
+    const participants = match['info']['participants'];
+    for (const player of participants) {
+        const team = config['teams'][String(player['teamId'])];
+        output += `${team} **${player['summonerName']}** *${player['championName']}* `;
+        output += `${player['kills']}/${player['deaths']}/${player['assists']}`;
+        output += '\n';
+    }
+    return output;
 }
