149974056af1ee4e80ba42e2e5d83feb0505dd8b
A. Tricot <al.tricot1@gmail.com>
2021-09-30 12:28:25 +0200

    Improve the output

--- handleMessage.js
+++ handleMessage.js
@@ -22,14 +22,21 @@ function lastGameBySummonerName(message, leagueJs, summonerName) {
     leagueJs.Summoner.gettingByName(summonerName).then((summ) => {
         leagueJs.Match.gettingListByAccount(summ.puuid, { beginIndex: 0, endIndex: 1 }).then(
             (data) => {
+                // initialize output
+                let output = '';
+                // get the last game
                 const last_match = data['matches'][0];
                 console.log(last_match['metadata']);
-                console.log(last_match['info']);
+                // print match metadata
+                const date = matchDate(last_match);
+                const winID = winningTeamID(last_match);
+                output += `Game from ${date}, `;
+                output += `${String(config['teams'][winID])} won\n\n`;
+                // print participants KDA
                 const participants = last_match['info']['participants'];
-                let output = '';
                 for (const player of participants) {
                     const team = config['teams'][String(player['teamId'])];
-                    output += `${team} ${player['summonerName']} ${player['championName']} `;
+                    output += `${team} **${player['summonerName']}** *${player['championName']}* `;
                     output += `${player['kills']}/${player['deaths']}/${player['assists']}`;
                     output += '\n';
                 }
@@ -37,4 +44,13 @@ function lastGameBySummonerName(message, leagueJs, summonerName) {
             },
         );
     });
+}
+
+function matchDate(match) {
+    return new Date(match['info']['gameCreation']);
+}
+
+function winningTeamID(match) {
+    if (match['info']['teams'][0]['win']) return match['info']['teams'][0]['teamId'];
+    else return match['info']['teams'][1]['teamId'];
 }
