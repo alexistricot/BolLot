829c097969322b6871295013b71aefaa8176e4e9
A. Tricot <al.tricot1@gmail.com>
2021-10-11 00:13:46 +0200

    Fix the currentMatchToString function.

--- currentMatchToString.js
+++ currentMatchToString.js
@@ -1,23 +1,19 @@
 module.exports = currentMatchToString;
 
+const data = require('./data.json');
+
 function currentMatchToString(match, leagueJs) {
     console.log(match);
     // initialize output
     let output = '';
-    // match metadata
-    const date = matchDate(match);
-    output += `Game from ${date}, `;
+    output += `Game started !, `;
     // participants KDA
     const participants = match['participants'];
     for (const player of participants) {
-        const team = match['teams'][String(player['teamId'])];
+        const team = data['teams'][String(player['teamId'])];
         const champ = leagueJs.StaticData.gettingChampionById(player['championId']);
         output += `${team} **${player['summonerName']}** *${champ}* `;
         output += '\n';
     }
     return output;
-}
-
-function matchDate(match) {
-    return new Date(match['info']['gameCreation']);
 }
