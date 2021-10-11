70286e0e3d80f4e0486c3fd54659cfea17155fb0
A. Tricot <al.tricot1@gmail.com>
2021-10-12 00:27:33 +0200

    First working version of the game tracker.

--- currentMatchToString.js
+++ currentMatchToString.js
@@ -1,19 +1,39 @@
 module.exports = currentMatchToString;
 
 const data = require('./data.json');
+const config = require('./config.json');
 
-function currentMatchToString(match, leagueJs) {
+function currentMatchToString(match, leagueJs, discordClient) {
     console.log(match);
     // initialize output
     let output = '';
-    output += `Game started !, `;
-    // participants KDA
+    output += `Game started !\n`;
+    // participants
     const participants = match['participants'];
+    const summonerName = [];
+    const championId = [];
+    const team = [];
     for (const player of participants) {
-        const team = data['teams'][String(player['teamId'])];
-        const champ = leagueJs.StaticData.gettingChampionById(player['championId']);
-        output += `${team} **${player['summonerName']}** *${champ}* `;
-        output += '\n';
+        team.push(data['teams'][String(player['teamId'])]);
+        championId.push(String(player['championId']));
+        summonerName.push(String(player['summonerName']));
     }
-    return output;
+    const promises = championId.map((x) => leagueJs.StaticData.gettingChampionById(x));
+    Promise.all(promises).then((champion) => {
+        console.log(champion);
+        for (let i = 0; i < summonerName.length; i++) {
+            output += `${team[i]} **${summonerName[i]}** *${champion[i]['name']}*\n`;
+        }
+        const channel = getChannel(discordClient);
+        send_string(channel, output);
+    });
+}
+
+function send_string(channel, output_str) {
+    console.log(`output_str: ${output_str}\n`);
+    channel.send(output_str);
+}
+
+function getChannel(discordClient) {
+    return discordClient.channels.cache.find((chan) => chan.id == config['channel']);
 }
--- listenForGames.js
+++ listenForGames.js
@@ -6,16 +6,7 @@ const currentMatchToString = require('./currentMatchToString');
 const fs = require('fs');
 
 function listenForGames(leagueJs, discordClient) {
-    // const scheduler = new ToadScheduler();
-    // check if players have a current game
-    // const getTask = new AsyncTask(
-    //     'get current games',
-    //     getCurrentGames(leagueJs, discordClient),
-    //     errorHandling,
-    // );
-    // const getJob = new SimpleIntervalJob({ seconds: config['interval'] }, getTask);
-    // scheduler.addSimpleIntervalJob(getJob);
-    setInterval(getCurrentGames(leagueJs, discordClient), config['interval']);
+    setInterval(getCurrentGames(leagueJs, discordClient), 1000 * config['interval']);
 }
 
 function errorHandling(err) {
@@ -27,28 +18,23 @@ function errorHandling(err) {
 
 function getCurrentGames(leagueJs, discordClient) {
     return () => {
-        const tracker = require('./tracker.json');
+        const tracker = JSON.parse(fs.readFileSync('./tracker.json'));
         for (const player in tracker['players']) {
             const summonerId = tracker['players'][player];
             leagueJs.Spectator.gettingActiveGame(summonerId)
                 .then((match) => {
-                    if (~tracker['games'].includes(match.gameId)) {
+                    if (!tracker['games'].includes(match.gameId)) {
                         console.log('\n\n');
                         console.log(match);
                         console.log('\n\n');
-                        const output_str = currentMatchToString(match, leagueJs);
-                        console.log(`output_str: ${output_str}\n`);
-                        discordClient
-                            .fetch(config['channel'])
-                            .then((channel) => channel.send(output_str))
-                            .catch(errorHandling);
-                        tracker['games'].push(match.id);
+                        tracker['games'].push(match.gameId);
                         fs.writeFile('tracker.json', JSON.stringify(tracker), errorHandling);
+                        currentMatchToString(match, leagueJs, discordClient);
                     }
                 })
                 .catch((err) => {
-                    if (err['error'].includes('Data not found')) return;
-                    console.log(err['error']);
+                    console.log(err);
+                    if (err & err.error & err?.error.includes('Data not found')) return;
                 });
         }
     };
