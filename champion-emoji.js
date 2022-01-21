f4467864e8788bced0ac5705adbf6077ccab197d
A. Tricot <al.tricot1@gmail.com>
2022-01-21 20:22:10 +0100

    Add champion emojis handling.

---
+++ champion-emoji.js
@@ -0,0 +1,36 @@
+const config = require('./config.json');
+const requestPromise = require('request-promise');
+
+module.exports = getChampionEmoji;
+
+uri = 'http://ddragon.leagueoflegends.com/cdn/12.2.1/img/champion/';
+
+async function getChampionEmoji(client, leagueJs, championId) {
+    // get the champion object form static data
+    const champion = await leagueJs.StaticData.gettingChampionById(championId);
+    // build the emoji name for this champion
+    const emojiName = getChampionEmojiName(champion.name);
+    // get the guild
+    const guild = client.guilds.cache.find((g) => g.id == config['guild']);
+    // get the emoji for this champion (if it exists)
+    let emoji = guild.emojis.cache.find((e) => e.name == emojiName);
+    if (~emoji) {
+        emoji = await createEmoji(guild, champion);
+    }
+    return emoji;
+}
+
+function getChampionEmojiName(championName) {
+    return championName.toLowerCase().replace(' ', '-');
+}
+
+function getChampionUrl(champion) {
+    return uri + champion.image.full;
+}
+
+async function createEmoji(guild, champion) {
+    const imageUrl = getChampionUrl(champion);
+    const emojiName = getChampionEmojiName(champion.name);
+    await guild.emojis.create(imageUrl, emojiName);
+    return guild.emojis.cache.find((e) => e.name == emojiName);
+}
